b48df9806c89df5079017ced8e93d43716ff9230
선진 <s22013@gsm.hs.kr>
2023-10-10 08:30:52 +0900

    interval 전에 실행 추가

--- subscribe.go
+++ subscribe.go
@@ -1,60 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "time"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 const (
-    baseUrl = "https://medium.com/feed/tag"
+	baseUrl = "https://medium.com/feed/tag"
 )
 
 var (
-    latestArticles = make(map[string]time.Time)
+	latestArticles = make(map[string]time.Time)
 )
 
 func watchArticleExpired() {
-    ticker := time.NewTicker(time.Minute)
-    for range ticker.C {
-        now := time.Now()
+	ticker := time.NewTicker(time.Minute)
+	for range ticker.C {
+		now := time.Now()
 
-        for title, expiresAt := range latestArticles {
-            if now.After(expiresAt) {
-                delete(latestArticles, title)
-            }
-        }
-    }
+		for title, expiresAt := range latestArticles {
+			if now.After(expiresAt) {
+				delete(latestArticles, title)
+			}
+		}
+	}
+}
+
+func doSubscribe(url string, articleStream chan<- article) {
+	resp, err := http.Get(url)
+	if err != nil {
+		// might have to handle error
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	articles, err := parseArticles(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, article := range articles {
+		if _, exists := latestArticles[article.title]; !exists {
+			latestArticles[article.title] = time.Now().Add(time.Hour)
+			articleStream <- article
+		}
+	}
 }
 
 func subscribe(category string, articleStream chan<- article, ticker *time.Ticker) {
-    url := fmt.Sprintf("%s/%s", baseUrl, category)
+	url := fmt.Sprintf("%s/%s", baseUrl, category)
 
-    for tick := range ticker.C {
-        func() {
-            resp, err := http.Get(url)
-            if err != nil {
-                // might have to handle error
-                panic(err)
-            }
+	doSubscribe(url, articleStream)
 
-            defer resp.Body.Close()
-
-            articles, err := parseArticles(resp.Body)
-            if err != nil {
-                panic(err)
-            }
-
-            took := time.Since(tick).Round(time.Millisecond)
-            log.Printf("task with category: %s took: %d ms\n", category, int(took.Milliseconds()))
-
-            for _, article := range articles {
-                if _, exists := latestArticles[article.title]; !exists {
-                    latestArticles[article.title] = time.Now().Add(time.Hour)
-                    articleStream <- article
-                }
-            }
-        }()
-    }
+	for tick := range ticker.C {
+		doSubscribe(url, articleStream)
+		took := time.Since(tick).Round(time.Millisecond)
+		log.Printf("task with category: %s took: %d ms\n", category, int(took.Milliseconds()))
+	}
 }
